17b7f5119b08af4c23d294b1610a7314284d767d
Bishwo Bijaya Shah <iworld023@gmail.com>
2022-08-07 22:27:29 +0545

    Add a address datatype

---
+++ danam/danam/datatypes/address.py
@@ -0,0 +1,43 @@
+from arches.app.datatypes.base import BaseDataType
+
+details = {
+    'datatype': 'address',
+    'iconclass': 'fa fa-location-arrow',
+    'modulename': 'datatypes.py',
+    'classname': 'AddressDataType',
+    'defaultwidget': None,
+    'defaultconfig': None,
+    'configcomponent': None,
+    'configname': None,
+    'isgeometric': False,
+    'issearchable': False
+}
+
+
+class AddressDataType(BaseDataType):
+
+    def validate(self, value, source=None):
+        errors = []
+        message = 'datatype: address, value: {1} {2} - missing required properties. This data was not imported.'
+        try:
+            value['address']
+            value['x']
+            value['y']
+        except KeyError:
+            errors.append({
+                'type': 'ERROR',
+                'message': message.format(value, source)
+            })
+
+        return errors
+
+    def append_to_document(self, document, nodevalue, nodeid, tile):
+        document['strings'].append({
+            'string': nodevalue['address'],
+            'nodegroup_id': tile.nodegroup_id
+        })
+
+    def get_search_terms(self, nodevalue, nodeid=None):
+        return [
+            nodevalue['address']
+        ]
